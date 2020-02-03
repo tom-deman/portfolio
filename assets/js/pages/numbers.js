f818ef0b11aede49e8de11de13c4631b0271a6ab
Thomas <dt.deman.thomas@gmail.com>
2020-02-03 12:01:02 +0100

    numbers.js done

--- assets/js/pages/numbers.js
+++ assets/js/pages/numbers.js
@@ -1,66 +1,71 @@
 let alreadyDone = false
 
+
 const numberOne   = document.querySelector( '.number-1' )
 const numberTwo   = document.querySelector( '.number-2' )
-const numberThree = document.querySelector( 'number-3' )
-const numberFour  = document.querySelector( 'number-4' )
+const numberThree = document.querySelector( '.number-3' )
+const numberFour  = document.querySelector( '.number-4' )
+
+
+let startNumberOne   = 0
+let endNumberOne     = 1060
+
+let startNumberTwo   = 0
+let endNumberTwo     = 60
+
+let startNumberThree = 0
+let endNumberThree   = 22
+
+let startNumberFour  = 0
+let endNumberFour    = 80
+
 
 window.addEventListener( 'scroll', () => {
     const ready = window.scrollY >= 550
 
+
     if( ready && !alreadyDone ) {
         alreadyDone = true
 
-        const startNumberOne = 0
-        const endNumberOne = 1060
-
-        const startNumberTwo = 0
-        const endNumberTwo = 60
-
-        const startNumberThree = 0
-        const endNumberThree = 22
-
-        const startNumberFour = 0
-        const endNumberFour = 80
-
-        const incrementOne = () => 
+        const incrementOne = () =>
             setTimeout(() => {
                 startNumberOne ++
                 if( startNumberOne <= endNumberOne ) {
-                    numberOne.innerHTML = startNumberOne
+                    numberOne.innerText = startNumberOne
                     incrementOne()
                 }
-            }, 1)
-        incrementOne()
+            }, 1 )
 
         const incrementTwo = () =>
             setTimeout(() => {
                 startNumberTwo ++
                 if( startNumberTwo <= endNumberTwo ) {
-                    numberTwo.innerHTML = startNumberTwo
+                    numberTwo.innerText = startNumberTwo
                     incrementTwo()
                 }
-            }, 65)
-        incrementTwo()
+            }, 65 )
 
         const incrementThree = () =>
             setTimeout(() => {
                 startNumberThree ++
                 if( startNumberThree <= endNumberThree ) {
-                    numberThree.innerHTML = startNumberThree
+                    numberThree.innerText = startNumberThree
                     incrementThree()
                 }
-            }, 180)
-        incrementThree()
+            }, 180 )
 
         const incrementFour = () =>
             setTimeout(() => {
                 startNumberFour ++
                 if( startNumberFour <= endNumberFour ) {
-                    numberFour.innerHTML = startNumberFour
+                    numberFour.innerText = startNumberFour
                     incrementFour()
                 }
-            }, 50)
-        incrementFour()
-    }
+            }, 50 )
+
+            incrementOne()
+            incrementTwo()
+            incrementThree()
+            incrementFour()
+        }
 })
